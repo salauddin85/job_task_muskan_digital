ca888018c46d924fe5387807b10e3d8325cdeee7
salauddin85 <ahmedsalauddin677785@gmail.com>
2024-11-27 22:09:06 +0600

    done

--- alis_project/settings.py
+++ alis_project/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'alis_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
